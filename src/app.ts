e346f86d246a9930ed86845528a6c1cfb434f0f3
Muhammad A Faishal <maafaishal@gmail.com>
2024-12-07 12:07:02 +0700

    feat: init

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+import "dotenv/config";
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("Here");
+  res.send({
+    title: "First",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`[server] Server is running on port ${PORT}`);
+});
